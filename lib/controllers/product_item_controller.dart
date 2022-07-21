2412e289fd2500d38cba0e9507592f7bca8fae36
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-07-21 05:31:29 +0530

    Deprecated...

--- lib/controllers/product_item_controller.dart
+++ lib/controllers/product_item_controller.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
-import '../views/product_detail_page.dart';
 import '../models/product.dart';
 import '../models/cart.dart';
 
@@ -12,43 +10,55 @@ class ProductItem extends StatelessWidget {
   Widget build(BuildContext context) {
     final product = Provider.of<Product>(context, listen: false);
     final cart = Provider.of<Cart>(context, listen: false);
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(10),
-      child: GridTile(
-        footer: GridTileBar(
-          backgroundColor: Colors.black87,
-          leading: Consumer<Product>(
-            builder: (ctx, product, _) => IconButton(
-              icon: Icon(
-                product.isFavorite ? Icons.favorite : Icons.favorite_border,
+    return Scaffold(
+      body: ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: GridTile(
+          footer: GridTileBar(
+            backgroundColor: Colors.black87,
+            leading: Consumer<Product>(
+              builder: (ctx, product, _) => IconButton(
+                icon: Icon(
+                  product.isFavorite ? Icons.favorite : Icons.favorite_border,
+                ),
+                color: Theme.of(context).colorScheme.secondary,
+                onPressed: () {
+                  product.toggleFavoriteStatus();
+                },
               ),
-              color: Theme.of(context).colorScheme.secondary,
+            ),
+            title: Text(
+              product.title,
+              textAlign: TextAlign.center,
+            ),
+            trailing: IconButton(
+              icon: const Icon(
+                Icons.shopping_cart,
+              ),
               onPressed: () {
-                product.toggleFavoriteStatus();
+                cart.addItem(product.id, product.price, product.title);
+                // ignore: deprecated_member_use
+                Scaffold.of(context).hideCurrentSnackBar();
+                // ignore: deprecated_member_use
+                Scaffold.of(context).showSnackBar(
+                  SnackBar(
+                    content: const Text(
+                      'Added item to cart!',
+                    ),
+                    duration: const Duration(seconds: 2),
+                    action: SnackBarAction(
+                      label: 'UNDO',
+                      onPressed: () {
+                        cart.removeSingleItem(product.id);
+                      },
+                    ),
+                    backgroundColor: Colors.red,
+                  ),
+                );
               },
+              color: Theme.of(context).colorScheme.error,
             ),
           ),
-          title: Text(
-            product.title,
-            textAlign: TextAlign.center,
-          ),
-          trailing: IconButton(
-            icon: const Icon(
-              Icons.shopping_cart,
-            ),
-            onPressed: () {
-              cart.addItem(product.id, product.price, product.title);
-            },
-            color: Theme.of(context).colorScheme.secondary,
-          ),
-        ),
-        child: GestureDetector(
-          onTap: () {
-            Navigator.of(context).pushNamed(
-              ProductDetailScreen.routeName,
-              arguments: product.id,
-            );
-          },
           child: Image.network(
             product.imageUrl,
             fit: BoxFit.cover,
@@ -58,3 +68,32 @@ class ProductItem extends StatelessWidget {
     );
   }
 }
+
+/**
+ * GestureDetector(
+            onTap: () {
+              //cart.addItem(product.id, product.price, product.title);
+            },
+            child: Image.network(
+              product.imageUrl,
+              fit: BoxFit.cover,
+            ),
+          ),
+ * 
+ ScaffoldMessenger.of(context).hideCurrentSnackBar();
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: const Text(
+                    'Added item to cart!',
+                  ),
+                  duration: const Duration(seconds: 2),
+                  action: SnackBarAction(
+                    label: 'UNDO',
+                    onPressed: () {
+                      cart.removeSingleItem(product.id);
+                    },
+                  ),
+                ),
+              );
+ * 
+ */
